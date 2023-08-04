727df621fccc5f4ff790167f703c54a079796d00
Crowdbotics <team@crowdbotics.com>
2023-08-04 20:03:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tracy-moose-lodge-2-42650",
+  url: "https://tracy-moose-lodge-2-42650.botics.co",
+  api: "https://tracy-moose-lodge-2-42650.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
